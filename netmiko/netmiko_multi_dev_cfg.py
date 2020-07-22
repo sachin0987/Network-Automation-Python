ed645cd47b7957bc0669fbd65d27be6872e58940
Sachin Verma <vsachin094@gmail.com>
2020-07-22 12:35:01 +0530

    Update netmiko_multi_dev_cfg.py

--- netmiko/netmiko_multi_dev_cfg.py
+++ netmiko/netmiko_multi_dev_cfg.py
@@ -1,6 +1,4 @@
-#!/usr/bin/python3
-# --------------------------------------------------------------
-#(c) 2017 Todd Riemenschneider
+
 # This script will allow you to configure multiple devices with
 # different configurations on each
 # --------------------------------------------------------------
